32c872072f30ccc38f96181301c0d2bf09351cb0
Paulo Mello <paulo@kenzie.com.br>
2022-07-11 15:00:24 -0300

    style: Input estilizado

--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import { css, styled } from "styled-components";
 
 export const InputContainer = styled.div`
   display: flex;
@@ -10,6 +10,35 @@ export const StyledLabel = styled.label`
 `;
 
 export const StyledInput = styled.input`
-  height: 48px;
   background-color: var(--grey2);
+  height: 3rem;
+  padding: 0 1rem; //mudei as medidas para rem e coloquei o padding
+  border: solid 1px var(--grey-scale-2);
+  border-color: ${(props) =>
+    props.error ? css`var(--primaryColor-negative)` : css`var(--grey-scale-2)`};
+  border-radius: 0.25rem;
+  color: var(--grey-scale-0);
+
+  @media (min-width: 600px) {
+    margin: 1.375rem 0;
+
+    &::placeholder {
+      color: var(--grey-scale-1);
+      margin-left: 1rem;
+    }
+    &:focus {
+      border: solid 1px var(--grey-scale-0);
+
+      &::placeholder {
+        color: var(--grey-scale-0);
+      }
+    }
+  }
+`;
+
+export const HelperText = styled.span`
+  margin-bottom: 0.5rem;
+  font-size: 0.75rem;
+  color: ${(props) =>
+    props.error ? css`var(--primaryColor-negative)` : css`var(--grey-scale-1)`};
 `;
